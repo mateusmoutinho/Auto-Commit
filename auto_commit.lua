e7f0b324b7ca8cc188d63b2e48b23608c938da53
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-05-22 00:55:43 -0300

    att

--- auto_commit.lua
+++ auto_commit.lua
@@ -23,6 +23,7 @@ function get_sugestion()
 
     llm.add_user_prompt("list the src dir, and explain what's inside")
     response = llm.generate()
+    dtw.remove_any(".commit_changes.txt")
     return sugestion
 end 
 
